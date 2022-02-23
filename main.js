3f724dbf57437b3b1e7b8b787011dea7ec3bf743
saiid <saiidouerttani@gmail.com>
2022-02-23 15:40:49 +0100

    loop section

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
-let zero = 0;
+let myAdmins = ["Ahmed","Jamel","Osama", "Sayed", "Stop", "Samera"];
+let myEmployees = [
+    "Amgad",
+    "Samah",
+    "Ameer",
+    "Jamila",
+    "Jalel",
+    "Omar",
+    "Othman",
+    "Sami",
+    "Amany",
+    "Samia",
+    "Anwar",
+];
+let numadmin = 2;
+document.write(`<div>We Have ${numadmin} Admins</div>`);
+document.write(`<hr>`);
+let emplyoswithadmin = [];
+for (let i = 0; i < myAdmins.length; i++) {
+    document.write(`<p>the admin for team ${i + 1} is ${myAdmins[i]}</p>`);
+    let firstletter = myAdmins[i][0];
+    for (let j = 0; j < myEmployees.length; j++) {
+        if (myEmployees[j][0] == firstletter) {
+            emplyoswithadmin.push(myEmployees[j]);
+        }
+    }
+    for (k = 0; k < emplyoswithadmin.length; k++) {
+        document.write(`<p>-${k + 1} ${emplyoswithadmin[k]} </p>`);
+    }
+    emplyoswithadmin.splice(0);
 
-let counter = 3;
-
-let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
-
-let my1 = my
-    .slice(zero, counter - true)
-    .sort()
-    .reverse();
-
-let my2 = my
-    .slice(counter - true, counter + true)
-    .sort()
-    .reverse();
-my = my2.concat(my1);
-console.log(my); // ["Osama", "Elham", "Mazero", "Ahmed"];
-
-console.log(my.slice(zero + true, counter)); // ["Elham", "Mazero"]
-
-my = my.slice(zero + true, counter);
-let str1 = my[zero].slice(zero, counter - true);
-let str2 = my[zero + true].slice(counter - true);
-
-console.log(str1 + str2); // "Elzero"
-str = str1 + str2;
-strfinal = str.slice(counter + true);
-console.log(strfinal[zero] + strfinal[zero + true].toLocaleUpperCase()); // "rO"
+    if (i == numadmin) {
+        break;84
+        
+    }
+}
