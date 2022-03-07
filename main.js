337ae4091a9de47d4938107de43fb26431c031dc
saiid <saiidouerttani@gmail.com>
2022-03-07 16:54:20 +0100

    bom challenge

--- main.js
+++ main.js
@@ -1,39 +1,45 @@
-document.forms[0].style = "width:500px; margin: 20px auto;";
+let input = document.querySelector("input");
+let button = document.querySelector(".add");
+button.style = "cursor:clickable";
+let list = document.querySelector(".listelement");
+let tabobj = [];
 
-let input = document.querySelectorAll("input");
+button.onclick = function () {
+  window.localStorage.clear();
 
-input.forEach((element) => {
-  element.style =
-    "display :block;height:30px; width:100%;margin-top:20px;padding:0px;border:0px ";
+  if (input.value != "") {
+    let elementtoadd = createelement(input.value);
+    list.appendChild(elementtoadd);
+  }
+};
+list.addEventListener("click", function (e) {
+  let target = e.target;
+  console.log(target);
+
+  target.onclick = function () {
+    list.removeChild(target.parentElement);
+  };
 });
 
-curentelement = document.getElementById("curentelement");
-curentelement.textContent = "Curent element";
-curentelement.style =
-  "background-color:#777; text-align:center;height:40px ;line-height:40px; width:100% ;margin-top:20px";
+function createelement(text) {
+  let element = document.createElement("div");
+  element.style =
+    "display:flex; justify-content:space-between;width:400px;background-color:white;align-items:center;border-radius:10px;padding-right:10px";
+  let para = document.createElement("p");
+  para.textContent = text;
+  let del = document.createElement("span");
+  del.textContent = "Delete";
+  del.style =
+    "background-color:red; color white;height:fit-content;padding:5px 10px;border-radius:10px";
+  element.appendChild(para);
+  element.appendChild(del);
+  let obj = {
+    id: Math.random(),
+    title: text,
+  };
 
-let result = document.getElementsByClassName("result");
-console.log(result);
-result[0].style =
-  "background-color:#777; text-align:center;height:40px ;line-height:40px; width:100% ;margin-top:20px";
-input[0].onblur = function () {
-  let classadd = input[0].value.split(" ");
-
-  console.log(classadd);
-  for (i = 0; i < classadd.length; i++) {
-    curentelement.classList.add(classadd[i]);
-    let div = document.createElement("div");
-    div.textContent = classadd[i];
-    result[0].appendChild(div);
-  }
-  input[0].value = "";
-};
-input[1].onblur = function () {
-  let classadd = input[1].value.split(" ");
-
-  console.log(classadd);
-  for (i = 0; i < classadd.length; i++) {
-    curentelement.classList.remove(classadd[i]);
-  }
-  input[1].value = "";
-};
+  tabobj.push(obj);
+  console.log(tabobj);
+  window.localStorage.setItem("task", JSON.stringify(tabobj));
+  return element;
+}
